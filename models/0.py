31482999c5ad5841a248ef87a3251094dd3f5216
Jim Karsten <iiijjjiii@gmail.com>
2012-05-04 19:11:43 -0400

    Mod 11491 - Input: Ensure WEB2PY_SERVER_MODE is set in httpd environment.

--- models/0.py
+++ models/0.py
@@ -4,11 +4,15 @@
 Place any app wide initialization here. This code gets run before any other
 models.
 """
-import os
+# C0103: *Invalid name "%%s" (should match %%s)*
+# pylint: disable=C0103
+
 from gluon.custom_import import track_changes, is_tracking_changes
+from applications.shared.modules.environ import server_production_mode
 
 DEBUG = False
-if os.environ.get('WEB2PY_SERVER_MODE', 'live') == 'test':
+SERVER_MODE = server_production_mode(request)
+if SERVER_MODE == 'test':
     DEBUG = True
     if not is_tracking_changes():
         track_changes()
